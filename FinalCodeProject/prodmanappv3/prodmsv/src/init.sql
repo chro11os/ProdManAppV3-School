6f175448bd2725cd2114768a16333f43c8544c64
chrollos <106138109+chro11os@users.noreply.github.com>
2024-08-02 23:40:13 +0800

    Delete FinalCodeProject directory
    Deeleted for a new database

--- FinalCodeProject/prodmanappv3/prodmsv/src/init.sql
+++
@@ -1,5 +0,0 @@
-USE prodmsdb;
-INSERT INTO uom_data(id,name)VALUES(1, 'kg');
-INSERT INTO uom_data(id,name)VALUES(2,'oz');
-INSERT INTO uom_data(id,name)VALUES(3,'gram');
-INSERT INTO uom_data(id,name)VALUES(4,'pc');
